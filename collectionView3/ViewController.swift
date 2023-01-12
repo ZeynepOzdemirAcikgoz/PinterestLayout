e8663f0b2722cc3e59b683db5719ebec50ce62aa
Zeynep Özdemir Açıkgöz <zeynmir@gmail.com>
2023-01-12 10:28:01 +0300

    Initial Commit

---
+++ collectionView3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionView3
+//
+//  Created by Zeynep Özdemir Açıkgöz on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
